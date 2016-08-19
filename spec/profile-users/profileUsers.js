c214e9ab8ff4b8058ec916affb14bfc245227eaf
Taras <tskrsaiuk@gmail.com>
2016-08-19 18:08:43 +0300

    add editable fields

--- spec/profile-users/profileUsers.js
+++ spec/profile-users/profileUsers.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import style from '../style';
 import {Card, CardTitle, CardText, CardMedia, CardActions} from '../../components/card';
 import FontIcon from '../../components/font_icon';
+import Input from '../../components/input';
 import Button from '../../components/button';
 
 import {USER_LOGO} from '../custom-components/topbar.js';
@@ -12,6 +13,7 @@ import {
   CSS_PROFILE_USER,
   CSS_PROFILE_USER_CREATE_BUTTON,
   CSS_PROFILE_USER_CREATE_BUTTONS_BLOCK,
+  CSS_PROFILE_USER_INPUT,
   ICON_CREATE,
   ICON_DONE,
   ICON_CLOSE
@@ -20,21 +22,43 @@ import {
 class ProfileUsersContainer extends Component {
   constructor(props){
     super(props);
+    this._fields = {
+      'Department' : ' ',
+      'Phone Number' : ' ',
+      'Title' : ' ',
+      'Email' : ' ',
+      'Account Status' : ' ',
+      'Notice' : ' '
+    };
+
     this.state = {
       editable : false,
-      fields : {
-        'Department' : '',
-        'Phone Number' : '',
-        'Title' : '',
-        'Email' : '',
-        'Account Status' : '',
-        'Notice' : ''
-      }
+      fields : this._fields,
+      fieldsBeforeSaving : this._fields
     }
   }
-  toggleEdit(){
+  toggleEdit(bool){
     this.setState({
-      editable : !this.state.editable
+      editable : bool
+    })
+  }
+  saveOrClose(bool){
+    if(bool){
+      this.setState({
+        fields : Object.assign({}, this.state.fields, this.state.fieldsBeforeSaving)
+      });
+    } else {
+      this.setState({
+        fieldsBeforeSaving : this._fields
+      })
+    }
+    this.toggleEdit(false);
+  }
+  handleChange(e,label){
+    this.setState({
+      fieldsBeforeSaving : Object.assign({},this.state.fieldsBeforeSaving, {[label] : e})
+    }, ()=>{
+      console.log(this.state.fieldsBeforeSaving[label])
     })
   }
   renderFields(fields = {}){
@@ -42,19 +66,34 @@ class ProfileUsersContainer extends Component {
 
     return (
       <div>
-
+        {keys.map((key,index)=>this.renderField({value : fields[key], label : key, index : index}))}
       </div>
     )
   }
-  renderIconBlock(){
+
+  renderField(field = {}) {
     let {editable} = this.state;
     let {theme} = this.props;
-    let _props = {
+    if (editable) {
+      return (
+        <Input className={theme[CSS_PROFILE_USER_INPUT]} onChange={(e)=>this.handleChange(e,field.label)}
+               key={field.index} type={'text'} value={field.value} label={field.label}/>
+      )
+    } else {
+      return (
+        <CardTitle key={field.index} style={{width: '50%', float: 'left'}}
+                   title={field.value} subtitle={field.label}/>
+      )
+    }
+  }
+  renderIconBlock(){
+    var {theme} = this.props;
+    var _props = {
       floating: true,
       onClick: this.toggleEdit.bind(this),
       className : theme[CSS_PROFILE_USER_CREATE_BUTTON]
     };
-    if(!editable){
+    if(!this.state.editable){
       _props = Object.assign(_props, {icon : ICON_CREATE});
       return (
         <div className={theme[CSS_PROFILE_USER_CREATE_BUTTONS_BLOCK]}>
@@ -64,12 +103,13 @@ class ProfileUsersContainer extends Component {
     } else {
       return (
         <div className={theme[CSS_PROFILE_USER_CREATE_BUTTONS_BLOCK]}>
-          {this.renderButtonIcon(Object.assign(_props, {icon : ICON_DONE}))}
-          {this.renderButtonIcon(Object.assign(_props, {icon : ICON_CLOSE}))}
+          {this.renderButtonIcon(Object.assign(_props, {icon : ICON_DONE, onClick : (e)=>this.saveOrClose(true)}))}
+          {this.renderButtonIcon(Object.assign(_props, {icon : ICON_CLOSE, onClick : (e)=>this.saveOrClose(false)}))}
         </div>
       )
     }
   }
+
   renderButtonIcon(props){
     return (
       <Button {...props}/>
@@ -94,7 +134,7 @@ class ProfileUsersContainer extends Component {
 
           <CardText>
             <div>
-              <CardTitle style={{width: '50%', float: 'left'}} title={'Law Department'} subtitle={'Department'}>
+             {/* <CardTitle style={{width: '50%', float: 'left'}} title={'Law Department'} subtitle={'Department'}>
 
               </CardTitle>
               <CardTitle style={{width: '50%', float: 'left'}} title={'+1 333 88 99 00'} subtitle={'Phone Number'}>
@@ -113,7 +153,8 @@ class ProfileUsersContainer extends Component {
               </CardTitle>
               <CardTitle style={{width: '50%', float: 'left'}} title={'+1 333 88 99 00'} subtitle={'Phone Number'}>
 
-              </CardTitle>
+              </CardTitle>*/}
+              {this.renderFields(this.state.fieldsBeforeSaving)}
             </div>
 
 
